ef4adf7d3f217ed97f4dd276cbbf7d725fcd06ed
Haruna Kuramochi <piyo@HarunanoMacBook-Air.local>
2026-04-26 21:30:43 +0900

    Revert "fix: メールリンクからトップページへリダイレクトしてログイン状態でチャット再開"
    This reverts commit a3f97bc221ee04c1bdb2846c06765deb319e6650.

--- components/ChatWidgetLoader.tsx
+++ components/ChatWidgetLoader.tsx
@@ -1,39 +1,8 @@
 "use client";
 import dynamic from "next/dynamic";
-import { useSearchParams, useRouter, usePathname } from "next/navigation";
-import { Suspense, useEffect, useState } from "react";
 
 const ChatWidget = dynamic(() => import("./ChatWidget"), { ssr: false });
 
-function ChatWidgetWithSession() {
-  const searchParams = useSearchParams();
-  const router = useRouter();
-  const pathname = usePathname();
-  const [initialSessionId, setInitialSessionId] = useState<string | undefined>(undefined);
-  const [ready, setReady] = useState(false);
-
-  useEffect(() => {
-    const chatSession = searchParams.get("chatSession");
-    if (chatSession) {
-      setInitialSessionId(chatSession);
-      // URLからパラメータを除去してクリーンに
-      const params = new URLSearchParams(searchParams.toString());
-      params.delete("chatSession");
-      const newUrl = params.size > 0 ? `${pathname}?${params}` : pathname;
-      router.replace(newUrl, { scroll: false });
-    }
-    setReady(true);
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
-  if (!ready) return null;
-  return <ChatWidget initialSessionId={initialSessionId} />;
-}
-
 export default function ChatWidgetLoader() {
-  return (
-    <Suspense fallback={null}>
-      <ChatWidgetWithSession />
-    </Suspense>
-  );
+  return <ChatWidget />;
 }
